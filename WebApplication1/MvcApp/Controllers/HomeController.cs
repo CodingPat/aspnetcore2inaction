08a826425bfc73e44403ec645318572fc806a278
Patrick Dudjalija <patrick.dudjalija@gmail.com>
2020-03-08 19:30:26 +0100

    beginning chap04

---
+++ WebApplication1/MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Diagnostics;
+
+
+namespace MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            String myString = null;
+            myString.ToString();
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            
+        }
+
+
+    }
+}
